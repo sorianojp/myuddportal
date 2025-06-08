08e43465c9d8992d8a6a35481689e887fa29b41a
sorianojp <johnpaul.soriano.6197@gmail.com>
2025-06-08 19:57:19 +0800

    api

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', function (LoginRequest $request) {
+    $request->authenticate();
+
+    return response()->json([
+        'success' => true,
+        'user' => Auth::user(),
+    ]);
+});
