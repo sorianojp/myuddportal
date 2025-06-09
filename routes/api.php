7f1ff48bbe2d3302cb54cfc083b8ea8138ffb61c
sorianojp <johnpaul.soriano.6197@gmail.com>
2025-06-09 15:20:47 +0800

    update login api

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,11 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', function (LoginRequest $request) {
-    try {
-        $request->authenticate();
-
-        return response()->json([
-            'success' => true,
-            'user' => Auth::user(),
-        ]);
-    } catch (ValidationException $e) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Invalid credentials. Please check your ID and password.',
-        ], 422);
-    }
+    $request->authenticate();
+    return response()->json([
+        'success' => true,
+        'user' => Auth::user(),
+    ]);
 });
 
 Route::get('/payments', [PaymentApiController::class, 'index']);
